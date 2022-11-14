056ea4c93a7db453e5cf8ce9758ec8545fb5c151
Ragavendra Kumar R <rranjik@rranjik-mbp.local>
2022-11-13 18:12:00 -0800

    5.8

---
+++ cia/5.8/main.cpp
@@ -0,0 +1,28 @@
+#include <atomic>
+#include <thread>
+#include <assert.h>
+std::atomic<bool> x,y;
+std::atomic<int> z;
+
+void write_x_then_y(){
+	x.store(true, std::memory_order_relaxed);
+	y.store(true, std::memory_order_release);
+}
+void read_y_then_x(){
+	//while(!y.load(std::memory_order_acquire));
+	while(!y.load(std::memory_order_relaxed));
+	if(x.load(std::memory_order_relaxed))
+		z++;
+}
+int main(int argv, char** argc){
+	for(int i = 0; i<atoi(argc[1]); i++){
+		x = false;
+		y = false;
+		z = 0;
+		std::thread b(read_y_then_x);
+		std::thread a(write_x_then_y);
+		a.join();
+		b.join();
+		assert(z!=0);
+	}
+}
